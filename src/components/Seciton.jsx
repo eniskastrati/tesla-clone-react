94067d8d57032502c0de57435083d4fafd4bcc13
Enis Kastrati <enis@smartmile.eu>
2022-04-29 11:28:49 +0200

    #comment created a new usable component to use for home section

---
+++ src/components/Seciton.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Seciton() {
+  return (
+    <Wrap>
+        Section
+    </Wrap>
+
+  )
+}
+
+export default Seciton
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: orange;
+    backgroud-image: 
+
+`
